e576d1bbac21f3bdd321a705be819533d96aa401
Nishan <nishan@ekoapp.com>
2021-06-14 16:37:14 +0700

    Update package information

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "XSDK", targets: ["AeroplaneX", "EngineX"])
     ],
     targets: [
-        .binaryTarget(name: "AeroplaneX", url: "https://raw.githubusercontent.com/nrlnishan/SwiftPM-Test/master/xsdk.zip", checksum: "3863a856638beed369fc092dffa44e0380604e2136883b5dae5c7c0ee0b5c347"),
-        .binaryTarget(name: "EngineX", url: "https://raw.githubusercontent.com/nrlnishan/SwiftPM-Test/master/xsdk.zip", checksum: "3863a856638beed369fc092dffa44e0380604e2136883b5dae5c7c0ee0b5c347"),
+        .binaryTarget(name: "AeroplaneX", url: "https://raw.githubusercontent.com/nrlnishan/SwiftPM-Test/master/AeroplaneX.xcframework.zip", checksum: "5f096d84e49845bdca4fd13ca424c4567d6636f51d446e93ffccd98efd8c36d7"),
+        .binaryTarget(name: "EngineX", url: "https://raw.githubusercontent.com/nrlnishan/SwiftPM-Test/master/EngineX.xcframework.zip", checksum: "d6d175e40e68b0e56a8ee69e124fa3e6e4f3d04f71dc9dc0f20abee8e1032410"),
     ]
 )
 
